878efc5f04c945c9dd1dbbe31fa1ab7cef72c3eb
sliencio <itcodes@outlook.com>
2018-11-16 00:12:30 +0800

    add letter Cell

---
+++ Assets/Scripts/Core/Letter.cs
@@ -0,0 +1,71 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Letter : MonoBehaviour, IPointerDownHandler
+{
+
+	public Text letter;
+	//字母索引
+	public int letterIndex = 0;
+	Image bg;
+
+	private bool isSelect = false;
+	// Use this for initialization
+	void Start ()
+	{
+		bg = GetComponent<Image>();
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		SetSelect();
+	}
+	
+	/// <summary>
+	/// 设置子母
+	/// </summary>
+	/// <param name="letterStr"></param>
+	public void SetLetter(string letterStr)
+	{
+		letter.text = letterStr;
+	}
+	/// <summary>
+	/// 设置大小
+	/// </summary>
+	/// <param name="size"></param>
+	public void SetSize(Vector2 size)
+	{
+		GetComponent<RectTransform>().sizeDelta = size;
+	}
+	
+	/// <summary>
+	/// 获取选中状态
+	/// </summary>
+	/// <returns></returns>
+	public bool GetSelectStatus()
+	{
+		return isSelect;
+	}
+
+	public int GetLetterIndex()
+	{
+		return letterIndex;
+	}
+	
+	/// <summary>
+	/// 设置选中状态
+	/// </summary>
+	void SetSelect()
+	{
+		isSelect = !isSelect;
+		bg.color = isSelect ? Color.red : Color.white;
+	}
+
+	public void SetData(int index, string letterStr)
+	{
+		letterIndex = index;
+		SetLetter(letterStr);
+	}
+}
--- Assets/Scripts/Core/Main.cs
+++ Assets/Scripts/Core/Main.cs
@@ -2,77 +2,97 @@
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-public class Main : MonoBehaviour
-{
-    //行
-    private int m_nRowNum = 5;
-    //列
-    private int m_nColNum = 4;
-    private int m_nMaxRow = 5;
+public class Main : MonoBehaviour {
+	//行
+	private int m_nRowNum = 5;
+	//列
+	private int m_nColNum = 4;
+	private int m_nMaxRow = 5;
+	//背景展示
+	public Transform m_BgPan;
+	private GridLayoutGroup m_BgGridLayoutGroup;
+	//Cell
+	public Transform m_CellPan;
+	private GridLayoutGroup m_CellGridLayoutGroup;
+	
+	
+	private float m_PanMaxHeight;
+	private float m_fMargin = 0;
 
-    public Transform m_Pan;
-    private GridLayoutGroup m_GridLayoutGroup;
-    private float m_PanMaxHeight;
-    private float m_fMargin = 0;
-    
-    public GameObject m_PanBgCell;
-    private List<GameObject> m_PanBgCellList = new List<GameObject>();
+	public GameObject m_PanBgCell;
+	public GameObject m_Cell;
 
-    // Use this for initialization
-    void Start()
-    {
-        m_GridLayoutGroup = m_Pan.GetComponent<GridLayoutGroup>();
-        m_PanMaxHeight = m_Pan.GetComponent<RectTransform>().sizeDelta.y;
-        m_fMargin = m_GridLayoutGroup.padding.top;
-        InitLayout();
-    }
+	List<string> WordList = new List<string> { "abcde", "dgdsa", "gsads", "sdfgd" };
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
+	// Use this for initialization
+	void Start() {
+		m_BgGridLayoutGroup = m_BgPan.GetComponent<GridLayoutGroup>();
+		m_CellGridLayoutGroup = m_CellPan.GetComponent<GridLayoutGroup>();
+		m_PanMaxHeight = m_BgPan.GetComponent<RectTransform>().sizeDelta.y;
+		m_fMargin = m_BgGridLayoutGroup.padding.top;
+		InitLayoutBg();
+	}
 
-    void InitLayout()
-    {
-        var cellSideLenth = (m_PanMaxHeight - m_fMargin * 2) / m_nRowNum;
-        m_Pan.GetComponent<RectTransform>().sizeDelta = new Vector2(cellSideLenth * m_nColNum + m_fMargin * 2 ,m_PanMaxHeight);
-        m_GridLayoutGroup.cellSize = new Vector2(cellSideLenth,cellSideLenth);
-        FlushPanBgCell();
-    }
+	//初始化布局
+	void InitLayoutBg() {
+		var cellSideLenth = (m_PanMaxHeight - m_fMargin * 2) / m_nRowNum;
+		Vector2 setSize = new Vector2(cellSideLenth * m_nColNum + m_fMargin * 2, m_PanMaxHeight);
+		m_BgPan.GetComponent<RectTransform>().sizeDelta = setSize;
+		m_CellPan.GetComponent<RectTransform>().sizeDelta = setSize;
+		m_BgGridLayoutGroup.cellSize = new Vector2(cellSideLenth, cellSideLenth);
+		m_CellGridLayoutGroup.cellSize = new Vector2(cellSideLenth, cellSideLenth);
+		FlushPanBgCell();
+		FlushLetterCell();
+	}
 
-    void FlushPanBgCell()
-    {
-        if (m_Pan.childCount < (m_nRowNum * m_nColNum))
-        {
-            for (int i = m_Pan.childCount; i <= (m_nRowNum * m_nColNum); i++)
-            {
-                GameObject cell = Instantiate(m_PanBgCell) as GameObject;
-                cell.transform.SetParent(m_Pan);
-                cell.transform.localPosition = Vector3.zero;
-                cell.transform.localScale = Vector3.one;
-                
-            }
-        }
-        
-        for (int i = 0; i < m_Pan.childCount;  i++)
-        {
-            m_Pan.GetChild(i).gameObject.SetActive(i<(m_nRowNum * m_nColNum));
-        }
+	
+	//初始化背景格子
+	void FlushLetterCell() {
+		if (m_CellPan.childCount < (m_nRowNum * m_nColNum)) {
+			for (int i = m_CellPan.childCount; i <= (m_nRowNum * m_nColNum); i++) {
+				GameObject cell = Instantiate(m_Cell);
+				cell.transform.SetParent(m_CellPan);
+				cell.transform.localPosition = Vector3.zero;
+				cell.transform.localScale = Vector3.one;
+
+			}
+		}
+
+		var wordList = WordList.ToString();
+		for (int i = 0; i < m_CellPan.childCount; i++) {
+			m_CellPan.GetChild(i).gameObject.SetActive(i < (m_nRowNum * m_nColNum));
+			m_CellPan.GetChild(i).GetComponent<Letter>().SetData(i,wordList.Substring(i,1));
+		}
+	}
+
+	//初始化背景格子
+	void FlushPanBgCell() {
+		if (m_BgPan.childCount < (m_nRowNum * m_nColNum)) {
+			for (int i = m_BgPan.childCount; i <= (m_nRowNum * m_nColNum); i++) {
+				GameObject cell = Instantiate(m_PanBgCell);
+				cell.transform.SetParent(m_BgPan);
+				cell.transform.localPosition = Vector3.zero;
+				cell.transform.localScale = Vector3.one;
+
+			}
+		}
+
+		for (int i = 0; i < m_BgPan.childCount; i++) {
+			m_BgPan.GetChild(i).gameObject.SetActive(i < (m_nRowNum * m_nColNum));
+		}
 
 
-        for (int i = 0; i <= (m_nRowNum * m_nColNum); i++)
-        {
-            int index = i + 1;
-            int row = (index % m_nColNum != 0)?(index/m_nColNum +1): (index / m_nColNum);
-            int col = (index % m_nColNum == 0)?(m_nColNum): (index % m_nColNum);
-            m_Pan.GetChild(i).GetComponent<Image>().color = ((row + col) % 2 == 0)?Color.gray:Color.clear;
-        }
-    }
+		for (int i = 0; i <= (m_nRowNum * m_nColNum); i++) {
+			int index = i + 1;
+			int row = (index % m_nColNum != 0) ? (index / m_nColNum + 1) : (index / m_nColNum);
+			int col = (index % m_nColNum == 0) ? (m_nColNum) : (index % m_nColNum);
+			m_BgPan.GetChild(i).GetComponent<Image>().color = ((row + col) % 2 == 0) ? Color.gray : Color.clear;
+		}
+	}
 
-    public void DropDownListener(int index)
-    {
-        m_nRowNum = m_nMaxRow + index;
-        m_nColNum = m_nRowNum - 1;
-        InitLayout();
-    }
+	public void DropDownListener(int index) {
+		m_nRowNum = m_nMaxRow + index;
+		m_nColNum = m_nRowNum - 1;
+		InitLayoutBg();
+	}
 }
